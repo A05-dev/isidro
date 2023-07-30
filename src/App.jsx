96146fd27240935c068278ca2fc12b8deb770c48
Andrés Martínez <autummata@gmail.com>
2023-07-30 00:07:21 -0700

    Deployed base frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+import Title from "./components/Title/Title";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Navbar />
+      <div className="flex items-center justify-center h-screen">
+        <Title />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'blackground' : '#213547',
+    },
+    extend: {
+      spacing: {
+        '5px': '5px',
+      },
+    },
   },
   plugins: [],
-}
-
+};
